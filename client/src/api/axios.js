da162180740d0dcd8129bf30b37d5decc3872a6e
SUNITA SATPATHY <sunitasatpathy80@gmail.com>
2026-07-03 02:56:02 +0530

    chore: point frontend to live backend URL

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://sims-4njv.onrender.com/api',
 });
 
 // Interceptor: automatically attaches token with every request
